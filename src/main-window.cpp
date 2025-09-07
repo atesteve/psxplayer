55a69e86ae6056961271a6b3aea562e8c570a15d
Aitor Esteve <hito.prl@gmail.com>
2025-09-07 23:12:01 +0200

    Fix time formatting

--- src/main-window.cpp
+++ src/main-window.cpp
@@ -22,7 +22,7 @@ QString ms_to_string(std::chrono::milliseconds ms)
     int min = duration_cast<minutes>(ms).count();
     int secs = duration_cast<seconds>(ms).count() % 60;
 
-    return QString::asprintf("%02d:%2d", min, secs);
+    return QString::asprintf("%02d:%02d", min, secs);
 }
 
 constexpr auto NUM_CHANNELS = 24;
