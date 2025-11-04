6ef6f6ec5ca78d29543fd9a06c80c368d33e7476
Aitor Esteve <hito.prl@gmail.com>
2025-11-04 13:25:48 +0100

    std::function not needed

--- src/channel-mapper/channel-mapper.cpp
+++ src/channel-mapper/channel-mapper.cpp
@@ -3,29 +3,29 @@
 #include "null-mapper.h"
 
 #include <unordered_map>
-#include <functional>
 
 namespace {
 
-const std::unordered_map<std::string_view, std::function<std::unique_ptr<ChannelMapper>()>>
-    builder_map{
-        {
-            "Final Fantasy 7",
-            [] { return std::make_unique<NullMapper>(16); },
-        },
-        {
-            "Final Fantasy 8",
-            [] { return std::make_unique<SquareMapper>(0x80015fe0, 0xf4, 0x110); },
-        },
-        {
-            "Final Fantasy 9",
-            [] { return std::make_unique<SquareMapper>(0x800585e0, 0x118, 0x134); },
-        },
-        {
-            "Chrono Cross",
-            [] { return std::make_unique<SquareMapper>(0x8004d084, 0x108, 0x124); },
-        },
-    };
+using cm_ptr = std::unique_ptr<ChannelMapper>;
+
+const std::unordered_map<std::string_view, cm_ptr (*)()> builder_map{
+    {
+        "Final Fantasy 7",
+        [] -> cm_ptr { return std::make_unique<NullMapper>(16); },
+    },
+    {
+        "Final Fantasy 8",
+        [] -> cm_ptr { return std::make_unique<SquareMapper>(0x80015fe0, 0xf4, 0x110); },
+    },
+    {
+        "Final Fantasy 9",
+        [] -> cm_ptr { return std::make_unique<SquareMapper>(0x800585e0, 0x118, 0x134); },
+    },
+    {
+        "Chrono Cross",
+        [] -> cm_ptr { return std::make_unique<SquareMapper>(0x8004d084, 0x108, 0x124); },
+    },
+};
 } // namespace
 
 std::unique_ptr<ChannelMapper> ChannelMapper::build(std::string_view game_name)
