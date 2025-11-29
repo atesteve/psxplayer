8fc385a4923e383d1517f2f58843862856d7de2f
Aitor Esteve Alvarado <aitor@aesteve.es>
2025-11-30 00:49:08 +0100

    Fix windows build

--- src/audio.cpp
+++ src/audio.cpp
@@ -6,6 +6,7 @@
 #include <fmt/format.h>
 
 #include <thread>
+#include <chrono>
 
 using namespace std::literals;
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,18 @@
 #include "libupse/upse.h"
 
 #include <QApplication>
+#include <QtSystemDetection>
 
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_hints.h>
 
 int main(int argc, char* argv[])
 {
+#ifdef Q_OS_LINUX
     // Prefer pulseaudio in Linux over pipewire, the pipewire driver seems to get stuck when
     // changing audio devices sometimes.
     SDL_SetHint(SDL_HINT_AUDIO_DRIVER, "pulseaudio");
+#endif
     SDL_SetAppMetadata("PSXPlayer", "0.0.1", "psxplayer");
     SDL_InitSubSystem(SDL_INIT_AUDIO);
     upse_module_init();
