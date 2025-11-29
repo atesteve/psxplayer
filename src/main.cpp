9f1a52992f461d042bea958e5ed4ebc17b208744
Aitor Esteve <hito.prl@gmail.com>
2025-11-30 00:13:33 +0100

    Prefer pulseaudio over pipewire

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "main-window.h"
 #include "libupse/upse.h"
+
 #include <QApplication>
+
 #include <SDL3/SDL_init.h>
+#include <SDL3/SDL_hints.h>
 
 int main(int argc, char* argv[])
 {
+    // Prefer pulseaudio in Linux over pipewire, the pipewire driver seems to get stuck when
+    // changing audio devices sometimes.
+    SDL_SetHint(SDL_HINT_AUDIO_DRIVER, "pulseaudio");
     SDL_SetAppMetadata("PSXPlayer", "0.0.1", "psxplayer");
     SDL_InitSubSystem(SDL_INIT_AUDIO);
     upse_module_init();
