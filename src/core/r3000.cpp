9f3c2e3286f54c7a80eab4735c6a8f680e87ad2e
Aitor Esteve <hito.prl@gmail.com>
2025-12-09 19:56:09 +0100

    More work

--- src/core/r3000.cpp
+++ src/core/r3000.cpp
@@ -10,8 +10,8 @@
 #include <utility>
 #include <stdckdint.h>
 #include <optional>
-
-using namespace fmt::literals;
+#include <cstddef>
+#include <new>
 
 namespace {
 
@@ -59,10 +59,55 @@ uint32_t sign_extend_16(uint32_t x)
     return (int16_t)x;
 }
 
-uint32_t sign_extend_8(uint32_t x)
+template<typename C, typename T>
+auto* container_of_impl(T* t, size_t offset)
 {
-    return (int8_t)x;
+    using return_t = std::conditional_t<std::is_const_v<T>, const C, C>;
+    using byte_t = std::conditional_t<std::is_const_v<T>, const std::byte, std::byte>;
+
+    auto* byte_ptr = reinterpret_cast<byte_t*>(t);
+    byte_ptr -= offset;
+    return std::launder(reinterpret_cast<return_t*>(byte_ptr));
 }
+
+#define container_of(ptr, type, member) container_of_impl<type>(ptr, offsetof(type, member))
+
+// clang-format off
+struct GPRName {
+    [[maybe_unused]] static constexpr size_t r0{0};
+    [[maybe_unused]] static constexpr size_t at{1};
+    [[maybe_unused]] static constexpr size_t v0{2};
+    [[maybe_unused]] static constexpr size_t v1{3};
+    [[maybe_unused]] static constexpr size_t a0{4};
+    [[maybe_unused]] static constexpr size_t a1{5};
+    [[maybe_unused]] static constexpr size_t a2{6};
+    [[maybe_unused]] static constexpr size_t a3{7};
+    [[maybe_unused]] static constexpr size_t t0{8};
+    [[maybe_unused]] static constexpr size_t t1{9};
+    [[maybe_unused]] static constexpr size_t t2{10};
+    [[maybe_unused]] static constexpr size_t t3{11};
+    [[maybe_unused]] static constexpr size_t t4{12};
+    [[maybe_unused]] static constexpr size_t t5{13};
+    [[maybe_unused]] static constexpr size_t t6{14};
+    [[maybe_unused]] static constexpr size_t t7{15};
+    [[maybe_unused]] static constexpr size_t s0{16};
+    [[maybe_unused]] static constexpr size_t s1{17};
+    [[maybe_unused]] static constexpr size_t s2{18};
+    [[maybe_unused]] static constexpr size_t s3{19};
+    [[maybe_unused]] static constexpr size_t s4{20};
+    [[maybe_unused]] static constexpr size_t s5{21};
+    [[maybe_unused]] static constexpr size_t s6{22};
+    [[maybe_unused]] static constexpr size_t s7{23};
+    [[maybe_unused]] static constexpr size_t t8{24};
+    [[maybe_unused]] static constexpr size_t t9{25};
+    [[maybe_unused]] static constexpr size_t k0{26};
+    [[maybe_unused]] static constexpr size_t k1{27};
+    [[maybe_unused]] static constexpr size_t gp{28};
+    [[maybe_unused]] static constexpr size_t sp{29};
+    [[maybe_unused]] static constexpr size_t s8{30};
+    [[maybe_unused]] static constexpr size_t ra{31};
+};
+// clang-format on
 
 } // namespace
 
@@ -99,6 +144,7 @@ public:
 
 template<R3000CoreConfig c>
 struct R3000Core<c>::Private {
+    explicit Private();
 
     struct HWAlignmentCheck {
         static void enable()
@@ -127,7 +173,9 @@ struct R3000Core<c>::Private {
         ~HWAlignmentCheckGuard() { HWAlignmentCheck::disable(); }
     };
 
-    void run();
+    void run_instruction();
+
+    void set_branch_target(r3000_ptr_t target);
 
     void run_r_inst(uint32_t function, uint32_t rs, uint32_t rt, uint32_t rd, uint32_t shift);
     void run_ij_inst(uint32_t opcode, uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
@@ -164,6 +212,7 @@ struct R3000Core<c>::Private {
 
     void run_ij_j(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
     void run_ij_jal(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
+    void run_ij_branch(uint32_t offset);
     void run_ij_beq(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
     void run_ij_bne(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
     void run_ij_blez(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
@@ -188,14 +237,6 @@ struct R3000Core<c>::Private {
     void run_ij_swl(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
     void run_ij_sw(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
     void run_ij_swr(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
-    void run_ij_lwc0(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
-    void run_ij_lwc1(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
-    void run_ij_lwc2(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
-    void run_ij_lwc3(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
-    void run_ij_swc0(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
-    void run_ij_swc1(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
-    void run_ij_swc2(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
-    void run_ij_swc3(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
     void run_ij_unk(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target);
 
     template<std::integral Int>
@@ -229,22 +270,66 @@ struct R3000Core<c>::Private {
     r3000_ptr_t pc{};
 
     struct load_delay_slot {
-        R3000Core<c>::Private* self;
         uint32_t rt;
         uint32_t value;
+        int count{};
 
-        ~load_delay_slot() {
-            self->gpr[rt] = value;
+        ~load_delay_slot()
+        {
+            // Given that:
+            //  1. std::optional<load_delay_slot> is standard layout.
+            //  2. The cotained object in std::optional is at offset 0.
+            //  3. R3000Core<c>::Private itself is standard layout.
+            //  4. This destructor is always called on the `load_slot` data member.
+            // Then it should be safe to get the pointer to its container object using
+            // `container_of()`
+            auto* container = container_of(this, R3000Core<c>::Private, load_slot);
+            container->gpr[rt] = value;
         }
     };
 
     std::optional<load_delay_slot> load_slot;
+
+    int branch_slot_count = 0;
+    r3000_ptr_t branch_target{};
+
+    static_assert(std::is_standard_layout_v<decltype(load_slot)>);
+    static_assert(requires(decltype(load_slot)& b) { (void*)&b == (void*)&*b; });
 };
 
 template<R3000CoreConfig c>
-void R3000Core<c>::Private::run()
+R3000Core<c>::Private::Private()
+{
+    static_assert(std::is_standard_layout_v<R3000Core<c>::Private>);
+}
+
+template<R3000CoreConfig c>
+void R3000Core<c>::Private::set_branch_target(r3000_ptr_t target)
+{
+    if (branch_slot_count > 0) {
+        return;
+    }
+    branch_slot_count = 2;
+    branch_target = target;
+}
+
+template<R3000CoreConfig c>
+void R3000Core<c>::Private::run_instruction()
 {
     HWAlignmentCheckGuard align_check_guard{};
+
+    // Check load delay slot.
+    if (load_slot) {
+        load_slot->count++;
+        if (load_slot->count >= 2) {
+            load_slot.reset();
+        }
+    }
+
+    // Check branch slot
+    if (branch_slot_count > 0 && --branch_slot_count == 0) {
+        pc = branch_target;
+    }
 
     auto const raw_inst = read_mem<r3000_ptr_t>(pc);
     auto const opcode = raw_inst >> 26;
@@ -257,8 +342,12 @@ void R3000Core<c>::Private::run()
         auto const [target, _] = std::bit_cast<jump_inst_t>(raw_inst);
         run_ij_inst(opcode, rs, rt, imm, target);
     }
+
     // After execution of the instruction, set R0 back to 0 in case it was written.
     gpr[0] = 0;
+
+    // Advance PC.
+    pc += sizeof(uint32_t);
 }
 
 template<R3000CoreConfig c>
@@ -397,18 +486,18 @@ void R3000Core<c>::Private::run_ij_inst(uint32_t opcode,
         case 0x2d: return run_ij_unk  (rs, rt, imm, target);
         case 0x2e: return run_ij_swr  (rs, rt, imm, target);
         case 0x2f: return run_ij_unk  (rs, rt, imm, target);
-        case 0x30: return run_ij_lwc0 (rs, rt, imm, target);
-        case 0x31: return run_ij_lwc1 (rs, rt, imm, target);
-        case 0x32: return run_ij_lwc2 (rs, rt, imm, target);
-        case 0x33: return run_ij_lwc3 (rs, rt, imm, target);
+        case 0x30: return run_ij_unk  (rs, rt, imm, target);
+        case 0x31: return run_ij_unk  (rs, rt, imm, target);
+        case 0x32: return run_ij_unk  (rs, rt, imm, target);
+        case 0x33: return run_ij_unk  (rs, rt, imm, target);
         case 0x34: return run_ij_unk  (rs, rt, imm, target);
         case 0x35: return run_ij_unk  (rs, rt, imm, target);
         case 0x36: return run_ij_unk  (rs, rt, imm, target);
         case 0x37: return run_ij_unk  (rs, rt, imm, target);
-        case 0x38: return run_ij_swc0 (rs, rt, imm, target);
-        case 0x39: return run_ij_swc1 (rs, rt, imm, target);
-        case 0x3a: return run_ij_swc2 (rs, rt, imm, target);
-        case 0x3b: return run_ij_swc3 (rs, rt, imm, target);
+        case 0x38: return run_ij_unk  (rs, rt, imm, target);
+        case 0x39: return run_ij_unk  (rs, rt, imm, target);
+        case 0x3a: return run_ij_unk  (rs, rt, imm, target);
+        case 0x3b: return run_ij_unk  (rs, rt, imm, target);
         case 0x3c: return run_ij_unk  (rs, rt, imm, target);
         case 0x3d: return run_ij_unk  (rs, rt, imm, target);
         case 0x3e: return run_ij_unk  (rs, rt, imm, target);
@@ -458,12 +547,19 @@ void R3000Core<c>::Private::run_r_srav(uint32_t rs, uint32_t rt, uint32_t rd, ui
 }
 
 template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_r_jr(uint32_t rs, uint32_t rt, uint32_t rd, uint32_t shift)
-{}
+void R3000Core<c>::Private::run_r_jr(uint32_t rs, uint32_t, uint32_t, uint32_t)
+{
+    set_branch_target(gpr[rs]);
+}
 
 template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_r_jalr(uint32_t rs, uint32_t rt, uint32_t rd, uint32_t shift)
-{}
+void R3000Core<c>::Private::run_r_jalr(uint32_t rs, uint32_t, uint32_t rd, uint32_t)
+{
+    // TODO: this instruction traps immediately when gpr[rs] is an invalid address, and not after
+    // the delay slot is executed like other branch instructions.
+    gpr[rd] = pc + sizeof(uint32_t) * 2;
+    set_branch_target(gpr[rs]);
+}
 
 template<R3000CoreConfig c>
 void R3000Core<c>::Private::run_r_syscall(uint32_t rs, uint32_t rt, uint32_t rd, uint32_t shift)
@@ -508,7 +604,7 @@ void R3000Core<c>::Private::run_r_mult(uint32_t rs, uint32_t rt, uint32_t, uint3
 }
 
 template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_r_multu(uint32_t rs, uint32_t rt, uint32_t rd, uint32_t shift)
+void R3000Core<c>::Private::run_r_multu(uint32_t rs, uint32_t rt, uint32_t, uint32_t)
 {
     uint64_t const result = (uint64_t)gpr[rs] * gpr[rt];
     lo = result;
@@ -552,6 +648,7 @@ void R3000Core<c>::Private::run_r_add(uint32_t rs, uint32_t rt, uint32_t rd, uin
     int32_t result;
     auto const overflow = ckd_add(&result, signed_rs, signed_rt);
     if (overflow) {
+        HWAlignmentCheck::disable();
         throw OverflowException{"add", signed_rs, signed_rt};
     }
     gpr[rd] = result;
@@ -571,6 +668,7 @@ void R3000Core<c>::Private::run_r_sub(uint32_t rs, uint32_t rt, uint32_t rd, uin
     int32_t result;
     auto const overflow = ckd_sub(&result, signed_rs, signed_rt);
     if (overflow) {
+        HWAlignmentCheck::disable();
         throw OverflowException{"sub", signed_rs, signed_rt};
     }
     gpr[rd] = result;
@@ -625,28 +723,63 @@ void R3000Core<c>::Private::run_r_unk(uint32_t rs, uint32_t rt, uint32_t rd, uin
 {}
 
 template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_j(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
+void R3000Core<c>::Private::run_ij_j(uint32_t, uint32_t, uint32_t, uint32_t target)
+{
+    auto const delay_slot_addr = pc + sizeof(uint32_t);
+    auto const target_addr = (delay_slot_addr & 0xfc000000u) | (target << 2);
+    set_branch_target(target_addr);
+}
 
 template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_jal(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
+void R3000Core<c>::Private::run_ij_jal(uint32_t, uint32_t, uint32_t, uint32_t target)
+{
+    gpr[GPRName::ra] = pc + sizeof(uint32_t) * 2;
+    auto const delay_slot_addr = pc + sizeof(uint32_t);
+    auto const target_addr = (delay_slot_addr & 0xfc000000u) | (target << 2);
+    set_branch_target(target_addr);
+}
 
 template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_beq(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
+void R3000Core<c>::Private::run_ij_branch(uint32_t offset)
+{
+    int32_t signed_offset = sign_extend_16(offset);
+    signed_offset <<= 2;
+    set_branch_target(pc + sizeof(uint32_t) + signed_offset);
+}
 
 template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_bne(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
+void R3000Core<c>::Private::run_ij_beq(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t)
+{
+    if (gpr[rs] == gpr[rt]) {
+        run_ij_branch(imm);
+    }
+}
 
 template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_blez(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
+void R3000Core<c>::Private::run_ij_bne(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t)
+{
+    if (gpr[rs] != gpr[rt]) {
+        run_ij_branch(imm);
+    }
+}
 
 template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_bgtz(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
+void R3000Core<c>::Private::run_ij_blez(uint32_t rs, uint32_t, uint32_t imm, uint32_t)
+{
+    int32_t const signed_rs = gpr[rs];
+    if (signed_rs <= 0) {
+        run_ij_branch(imm);
+    }
+}
+
+template<R3000CoreConfig c>
+void R3000Core<c>::Private::run_ij_bgtz(uint32_t rs, uint32_t, uint32_t imm, uint32_t)
+{
+    int32_t const signed_rs = gpr[rs];
+    if (signed_rs > 0) {
+        run_ij_branch(imm);
+    }
+}
 
 template<R3000CoreConfig c>
 void R3000Core<c>::Private::run_ij_addi(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t)
@@ -656,6 +789,7 @@ void R3000Core<c>::Private::run_ij_addi(uint32_t rs, uint32_t rt, uint32_t imm, 
     int32_t result;
     auto const overflow = ckd_add(&result, signed_rs, signed_imm);
     if (overflow) {
+        HWAlignmentCheck::disable();
         throw OverflowException{"addi", signed_rs, signed_imm};
     }
     gpr[rt] = result;
@@ -708,40 +842,66 @@ void R3000Core<c>::Private::run_ij_lui(uint32_t, uint32_t rt, uint32_t imm, uint
 template<R3000CoreConfig c>
 void R3000Core<c>::Private::run_ij_lb(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t)
 {
-    load_slot.emplace(this, rt, read_mem<int8_t>(rs + sign_extend_16(imm)));
+    load_slot.emplace(rt, read_mem<int8_t>(gpr[rs] + sign_extend_16(imm)));
 }
 
 template<R3000CoreConfig c>
 void R3000Core<c>::Private::run_ij_lh(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t)
 {
-    load_slot.emplace(this, rt, read_mem<int16_t>(rs + sign_extend_16(imm)));
+    load_slot.emplace(rt, read_mem<int16_t>(gpr[rs] + sign_extend_16(imm)));
 }
 
 template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_lwl(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
+void R3000Core<c>::Private::run_ij_lwl(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t)
+{
+    auto const prev_value = load_slot
+                                .and_then([&](auto const& l) {
+                                    return l.rt == rt ? std::make_optional(l.value) : std::nullopt;
+                                })
+                                .value_or(gpr[rt]);
+    auto const addr = gpr[rs] + sign_extend_16(imm);
+    auto const misalignment = addr & 0x3;
+    auto const aligned_addr = addr & ~0x3;
+    auto const word = read_mem<uint32_t>(aligned_addr);
+    auto const mask = 0xffffffffu << ((4 - misalignment) * 8);
+    auto const result = (prev_value & mask) | (word << (misalignment * 8));
+    load_slot.emplace(rt, result);
+}
 
 template<R3000CoreConfig c>
 void R3000Core<c>::Private::run_ij_lw(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t)
 {
-    load_slot.emplace(this, rt, read_mem<uint32_t>(rs + sign_extend_16(imm)));
+    load_slot.emplace(rt, read_mem<uint32_t>(rs + sign_extend_16(imm)));
 }
 
 template<R3000CoreConfig c>
 void R3000Core<c>::Private::run_ij_lbu(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t)
 {
-    load_slot.emplace(this, rt, read_mem<uint8_t>(rs + sign_extend_16(imm)));
+    load_slot.emplace(rt, read_mem<uint8_t>(rs + sign_extend_16(imm)));
 }
 
 template<R3000CoreConfig c>
 void R3000Core<c>::Private::run_ij_lhu(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t)
 {
-    load_slot.emplace(this, rt, read_mem<uint16_t>(rs + sign_extend_16(imm)));
+    load_slot.emplace(rt, read_mem<uint16_t>(rs + sign_extend_16(imm)));
 }
 
 template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_lwr(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
+void R3000Core<c>::Private::run_ij_lwr(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t)
+{
+    auto const prev_value = load_slot
+                                .and_then([&](auto const& l) {
+                                    return l.rt == rt ? std::make_optional(l.value) : std::nullopt;
+                                })
+                                .value_or(gpr[rt]);
+    auto const addr = gpr[rs] + sign_extend_16(imm);
+    auto const misalignment = addr & 0x3;
+    auto const aligned_addr = addr & ~0x3;
+    auto const word = read_mem<uint32_t>(aligned_addr);
+    auto const mask = 0xffffffffu >> ((4 - misalignment) * 8);
+    auto const result = (prev_value & mask) | (word >> (misalignment * 8));
+    load_slot.emplace(rt, result);
+}
 
 template<R3000CoreConfig c>
 void R3000Core<c>::Private::run_ij_sb(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
@@ -764,38 +924,6 @@ void R3000Core<c>::Private::run_ij_swr(uint32_t rs, uint32_t rt, uint32_t imm, u
 {}
 
 template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_lwc0(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
-
-template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_lwc1(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
-
-template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_lwc2(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
-
-template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_lwc3(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
-
-template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_swc0(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
-
-template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_swc1(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
-
-template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_swc2(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
-
-template<R3000CoreConfig c>
-void R3000Core<c>::Private::run_ij_swc3(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
-{}
-
-template<R3000CoreConfig c>
 void R3000Core<c>::Private::run_ij_unk(uint32_t rs, uint32_t rt, uint32_t imm, uint32_t target)
 {}
 
@@ -810,7 +938,7 @@ R3000Core<c>::~R3000Core() = default;
 template<R3000CoreConfig c>
 void R3000Core<c>::run()
 {
-    p->run();
+    p->run_instruction();
 }
 
 // Explicit instantiation
